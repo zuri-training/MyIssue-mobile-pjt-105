4dd37c226ca66002d1a122c96edb53b75d9484ff
Sarah <sarahligbe12@gmail.com>
2021-07-03 16:36:18 +0100

    Added signup page Migrated to androidx

--- app/src/main/java/com/example/pjt105/SignupActivity.kt
+++ app/src/main/java/com/example/pjt105/SignupActivity.kt
@@ -3,9 +3,9 @@ package com.example.pjt105
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class LoginActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_signup)
     }
 }
